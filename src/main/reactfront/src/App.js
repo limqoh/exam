2fb82f0bbc885554cd97e902dd1fa2533cd6a085
LimKyuOh <limqoh@gmail.com>
2023-01-20 21:45:19 +0900

    commit

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  // message 초기값 설정 (""로 설정)
+  const [message, setMessage] = useState("");
+
+  // useEffect(함수, 배열) : 컴포넌트가 화면에 나타났을 때 자동 실행
+  useEffect(() => {
+    // fetch(url, options) : Http 요청 함수
+    fetch("/test")
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2> 취업 정보는 코코's 블로그에서 </h2>
+        <h2> ↓↓↓ </h2>
+        <a href={ message } target='_blank' rel='noreferrer' style={ {color:"yellow"} }>  <h2> 바로가기 [{ message }] </h2> </a>
       </header>
     </div>
   );
