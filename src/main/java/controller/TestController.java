154a6225465f2d0ca16fe39c30b88843016effe9
LimKyuOh <limqoh@gmail.com>
2023-01-21 12:09:36 +0900

    commit

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "https://brilliant-coco.tistory.com/";
+    }
 }
